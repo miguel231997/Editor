8eab69ed3c5e17b404d6393fb12e16e9f53fee0c
miguel231997 <miguelgalindo1214@gmail.com>
2017-04-10 15:48:37 -0400

    Update PrecondViolatedExcept.h

--- Editor/PrecondViolatedExcept.h
+++ Editor/PrecondViolatedExcept.h
@@ -1,8 +1,4 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
-/** Listing 7-5.
-    @file PrecondViolatedExcept.h */
 
 #ifndef PRECOND_VIOLATED_EXCEPT_
 #define PRECOND_VIOLATED_EXCEPT_
